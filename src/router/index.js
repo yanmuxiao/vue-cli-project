31e5680404b3ec8b1126294effe3ccf0ee2ed643
xlg-git <421559950@qq.com>
2017-08-23 23:16:27 +0800

    header

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import LoginVue from '@/views/Login.vue'
+import MainVue from '@/views/Main.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
+      path: '/',
+      name: 'MainVue',
+      component: MainVue
+    },{
       path: '/login',
       name: 'LoginVue',
       component: LoginVue
---
+++ src/views/Main.vue
@@ -0,0 +1,41 @@
+<template>  
+    <div class="main-vue">
+        <header class="header-menu"></header>
+       
+    </div>
+</template>
+
+
+<style  lang="scss">
+    .header-menu {
+        height: 60px;
+        width: 100%;
+        background-color: #f00;
+    }
+   
+</style>
+
+<script>
+
+    export default {
+        data() {
+            return {
+                
+            }
+        },
+        methods: {
+        },
+        created() {
+        },
+        computed: {
+        },
+        mounted () {
+        },
+        watch: {  
+        },
+        updated() {
+        }
+    }
+
+
+</script>
