54a1efa52f3266e2c2d11fb6a07bed48dcccedeb
yanmuxiao <412559950@qq.com>
2018-11-06 09:09:05 +0800

    publicM action.js

--- src/vuex/modules/publicM/actions.js
+++ src/vuex/modules/publicM/actions.js
@@ -7,7 +7,7 @@ export default {
 		console.log('arguments==>');
 		console.log(arguments);
 		console.log('commit==>');
-		console.log(commit);
+		console.log(commit); // 对象的结构获取commit方法
 		commit('LOADING', data);
 	},
 	mutation_action({commit}) {
